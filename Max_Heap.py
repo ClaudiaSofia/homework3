29e2e471458671dbc996cb9de78c09aa61507159
ClaudiaSofia <33122942+ClaudiaSofia@users.noreply.github.com>
2017-12-03 12:35:57 +0100

    Add files via upload

---
+++ Max_Heap.py
@@ -0,0 +1,50 @@
+class MaxBinHeap:
+    def __init__(self, array = []):
+        self.heapList = [0]
+        self.currentSize = 0
+        for i in array:
+            self.insert(i)
+
+    def insert(self, k):
+        self.heapList.append(k)
+        self.currentSize = self.currentSize + 1
+        self.percUp(self.currentSize)
+
+
+    def percUp(self, i):
+        while i // 2 > 0:
+            if self.heapList[i] > self.heapList[i // 2]:
+                tmp = self.heapList[i // 2]
+                self.heapList[i // 2] = self.heapList[i]
+                self.heapList[i] = tmp
+            i = i // 2
+
+    def getMax(self):
+        return self.heapList[1]
+
+    def delMax(self):
+        retval = self.heapList[1]
+        self.heapList[1] = self.heapList[self.currentSize]
+        self.currentSize = self.currentSize - 1
+        self.heapList.pop()
+        self.percDown(1)
+        return retval
+
+    def percDown(self, i):
+        while (i * 2) <= self.currentSize:
+            mc = self.maxChild(i)
+            if self.heapList[i] < self.heapList[mc]:
+                tmp = self.heapList[i]
+                self.heapList[i] = self.heapList[mc]
+                self.heapList[mc] = tmp
+            i = mc
+
+
+    def maxChild(self, i):
+        if i * 2 + 1 > self.currentSize:
+            return i * 2
+        else:
+            if self.heapList[i * 2] > self.heapList[i * 2 + 1]:
+                return i * 2
+            else:
+                return i * 2 + 1
